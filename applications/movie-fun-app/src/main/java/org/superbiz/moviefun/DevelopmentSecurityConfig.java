cd21306fb10ddbf12ee22381d7a8d26476cdfb82
Brad <t.bradley.owens@gmail.com>
2018-05-23 12:45:37 -0600

    service discovery

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/DevelopmentSecurityConfig.java
@@ -1,6 +1,7 @@
 package org.superbiz.moviefun;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
@@ -9,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 @ConditionalOnProperty(value = "application.oauth-enabled", havingValue = "false")
 public class DevelopmentSecurityConfig {
-
+    @LoadBalanced
     @Bean
     public RestOperations restOperations() {
         return new RestTemplate();
